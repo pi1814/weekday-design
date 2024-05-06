c1c784a01c4bc854673677e9640e10b12794f50c
pi1814 <parth.inamdar1@gmail.com>
2024-05-06 19:23:57 +0530

    Getting the structure down for the project

---
+++ weekday-design/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <JobFilters />
+        <JobList />
+      </div>
+  );
+}
+
+export default App;
